fc645ec7919f0ed6c9c0302497cac2243c4a2e29
Lucas Song <lucas7song@gmail.com>
2019-12-03 14:52:11 -0800

    add queries template

---
+++ sql/query.sql
@@ -0,0 +1,115 @@
+
+--    public static void addCustomer(DBProject esql){
+-- 	  // Given customer details add the customer in the DB 
+--       // Your code goes here.
+--       // ...
+--       // ...
+--    }//end addCustomer
+
+
+
+
+
+--    public static void addRoom(DBProject esql){
+-- 	  // Given room details add the room in the DB
+--       // Your code goes here.
+--       // ...
+--       // ...
+--    }//end addRoom
+
+--    public static void addMaintenanceCompany(DBProject esql){
+--       // Given maintenance Company details add the maintenance company in the DB
+--       // ...
+--       // ...
+--    }//end addMaintenanceCompany
+
+--    public static void addRepair(DBProject esql){
+-- 	  // Given repair details add repair in the DB
+--       // Your code goes here.
+--       // ...
+--       // ...
+--    }//end addRepair
+
+--    public static void bookRoom(DBProject esql){
+-- 	  // Given hotelID, roomNo and customer Name create a booking in the DB 
+--       // Your code goes here.
+--       // ...
+--       // ...
+--    }//end bookRoom
+
+--    public static void assignHouseCleaningToRoom(DBProject esql){
+-- 	  // Given Staff SSN, HotelID, roomNo Assign the staff to the room 
+--       // Your code goes here.
+--       // ...
+--       // ...
+--    }//end assignHouseCleaningToRoom
+   
+--    public static void repairRequest(DBProject esql){
+-- 	  // Given a hotelID, Staff SSN, roomNo, repairID , date create a repair request in the DB
+--       // Your code goes here.
+--       // ...
+--       // ...
+--    }//end repairRequest
+   
+--    public static void numberOfAvailableRooms(DBProject esql){
+-- 	  // Given a hotelID, get the count of rooms available 
+--       // Your code goes here.
+--       // ...
+--       // ...
+--    }//end numberOfAvailableRooms
+   
+--    public static void numberOfBookedRooms(DBProject esql){
+-- 	  // Given a hotelID, get the count of rooms booked
+--       // Your code goes here.
+--       // ...
+--       // ...
+--    }//end numberOfBookedRooms
+   
+--    public static void listHotelRoomBookingsForAWeek(DBProject esql){
+-- 	  // Given a hotelID, date - list all the rooms available for a week(including the input date) 
+--       // Your code goes here.
+--       // ...
+--       // ...
+--    }//end listHotelRoomBookingsForAWeek
+   
+--    public static void topKHighestRoomPriceForADateRange(DBProject esql){
+-- 	  // List Top K Rooms with the highest price for a given date range
+--       // Your code goes here.
+--       // ...
+--       // ...
+--    }//end topKHighestRoomPriceForADateRange
+   
+--    public static void topKHighestPriceBookingsForACustomer(DBProject esql){
+-- 	  // Given a customer Name, List Top K highest booking price for a customer 
+--       // Your code goes here.
+--       // ...
+--       // ...
+--    }//end topKHighestPriceBookingsForACustomer
+   
+--    public static void totalCostForCustomer(DBProject esql){
+-- 	  // Given a hotelID, customer Name and date range get the total cost incurred by the customer
+--       // Your code goes here.
+--       // ...
+--       // ...
+--    }//end totalCostForCustomer
+   
+--    public static void listRepairsMade(DBProject esql){
+-- 	  // Given a Maintenance company name list all the repairs along with repairType, hotelID and roomNo
+--       // Your code goes here.
+--       // ...
+--       // ...
+--    }//end listRepairsMade
+   
+--    public static void topKMaintenanceCompany(DBProject esql){
+-- 	  // List Top K Maintenance Company Names based on total repair count (descending order)
+--       // Your code goes here.
+--       // ...
+--       // ...
+--    }//end topKMaintenanceCompany
+   
+--    public static void numberOfRepairsForEachRoomPerYear(DBProject esql){
+-- 	  // Given a hotelID, roomNo, get the count of repairs per year
+--       // Your code goes here.
+--       // ...
+--       // ...
+--    }//end listRepairsMade
