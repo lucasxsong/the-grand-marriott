9703fd18c3d7f24ce1ad7b5793f9ff3db36793c0
Cameron <cmori007@ucr.edu>
2019-12-06 17:24:36 -0800

    Update ubuntu stop script

--- postgresql/stopPSQL.sh
+++ postgresql/stopPSQL.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-/usr/lib/postgresql/10/bin/pg_ctl -o "-c unix_socket_directories=$PGSOCKETS -p $PGPORT" -D $PGDATA -l $folder/logfile stop
+/usr/lib/postgresql/10/bin/pg_ctl stop -D /tmp/cam/myDB/data
